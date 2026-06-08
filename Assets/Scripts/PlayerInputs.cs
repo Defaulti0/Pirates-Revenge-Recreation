059eb53c8e411708b5ca42c7e9e7139fba9a3cb2
Christopher Martinez <65252323+Defaulti0@users.noreply.github.com>
2026-06-07 23:10:38 -0400

    Initial check-in

---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        // Init Vars
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // For normal stuff
+    }
+
+    void FixedUpdate() {
+        // For Physics or stuff that need consistency
+    }
+}
