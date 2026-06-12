92ed3e7c6e9b7939a0905b58f6de499f850f0a2d
Defaulti0 <crismartin994@gmail.com>
2026-06-11 22:50:37 -0400

    feat: implement player movement and projectile firing mechanics with new player controller and test scene

--- Assets/Scripts/PlayerInputs.cs
+++ Assets/Scripts/PlayerInputs.cs
@@ -1,22 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    // Variables
+    public float playerSpeed = 0.0f;
+    public int health = 100;
+    public bool isInvincible = false;
+    public float superMeter = 0.0f;
+
+    public GameObject projectile;
+    public float fireRate = 1.0f;
+
+    private Rigidbody rb;
+    private float movementX;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        // Init Vars
-        int playerSpeed = 5;
-        int health = 50;
-        bool isInvincible = false;
-        float superMeter = 0.0f;
-    }
+        rb = GetComponent<Rigidbody>();
 
-    // Update is called once per frame
-    void Update() {
-        // For normal stuff
     }
 
     void FixedUpdate() {
-        // For Physics or stuff that need consistency
+        Vector3 movement = new Vector3(movementX, 0.0f, 0.0f);
+        rb.AddForce(movement * playerSpeed);
     }
+
+    void OnMove (InputValue movementValue) {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+    }
+
+    void OnFire (InputValue fireValue) {
+        if (fireValue.isPressed) {
+            Instantiate(projectile, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
+        }
+    }
+
 }
--- Assets/auto_git_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-git add .
-git commit -m "Automated push on Unity close"
-git push
